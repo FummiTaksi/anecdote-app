e395233bb9a6ae9104cf38cd1e1376024524b838
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-02 22:16:26 +0200

    t19 tehty

--- src/index.js
+++ src/index.js
@@ -4,34 +4,48 @@ import './index.css';
 
 
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; 
   }
+
+  const makeAnecdoteObject = (text) => {
+      return ({
+          text: text,
+          votes: 0
+      })
+  }
+
 
 class App extends React.Component {
 
     constructor(props) {
       super(props)
       this.state = {
-        selected: 0
+        selected: 0,
       }
     }
 
      changeIndex = () => {
         return () => {
             this.setState({
-                selected: getRandomInt(0, anecdotes.length)
+                selected: getRandomInt(0, this.props.anecdotes.length)
             })
         }
+    }
+
+    addVoteToSelected = () => {
+        this.props.anecdotes[this.state.selected].votes += 1;
+        this.forceUpdate();
     }
   
     render() {
       return (
         <div>
-          {this.props.anecdotes[this.state.selected]}
-          <br/>
+          <p>{this.props.anecdotes[this.state.selected].text}</p>
+          <p>Has {this.props.anecdotes[this.state.selected].votes} votes</p>
+          <button onClick = {this.addVoteToSelected}>Vote</button>
           <button onClick = {this.changeIndex()}>Next anecdote</button>
         </div>
       )
@@ -39,12 +53,12 @@ class App extends React.Component {
   }
   
   const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    makeAnecdoteObject('If it hurts, do it more often'),
+    makeAnecdoteObject('Adding manpower to a late software project makes it later!'),
+    makeAnecdoteObject('The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.'),
+    makeAnecdoteObject('Any fool can write code that a computer can understand. Good programmers write code that humans can understand.'),
+    makeAnecdoteObject('Premature optimization is the root of all evil.'),
+    makeAnecdoteObject('Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.')
   ]
   
   ReactDOM.render(
